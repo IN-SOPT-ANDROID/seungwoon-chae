3c34f686cc70a566892ef3fd8ea639f81fe6ac63
CHAE <chaeseung-un@chaeseung-un-ui-MacBookAir.local>
2022-10-02 00:59:33 +0900

    [test] 첫번쨰

--- app/src/main/java/org/sopt/sample/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/MainActivity.kt
@@ -6,5 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 뭐야 이거 왜 컴포즈랑 xml이랑 따로 안 떨어지지?
     }
 }
