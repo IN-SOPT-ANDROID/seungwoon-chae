166073bd2ab2c16fd90347080dc2da3ff68c57b3
CHAE <chaso1495@naver.com>
2022-10-05 20:33:20 +0900

    [심화] flow 활용 버튼 조정

--- app/src/main/java/org/sopt/sample/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/SignUpActivity.kt
@@ -22,12 +22,12 @@ class SignUpActivity : AppCompatActivity() {
         with(binding)
         {
             btSignupend.setOnClickListener() {
-                if (etId.text.length >= 6 && etId.text.length <= 10) {
-                    if (etPw.text.length >= 8 && etPw.text.length <= 12) {
+                if (etIdNew.text.length >= 6 && etIdNew.text.length <= 10) {
+                    if (etPwNew.text.length >= 8 && etPwNew.text.length <= 12) {
                         Snackbar.make(root, "회원가입이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
-                        intent.putExtra("id", etId.text.toString())
-                        intent.putExtra("pw", etPw.text.toString())
-                        intent.putExtra("mbti", etMbti.text.toString())
+                        intent.putExtra("id", etIdNew.text.toString())
+                        intent.putExtra("pw", etPwNew.text.toString())
+                        intent.putExtra("mbti", etMbtiNew.text.toString())
                         startActivity(intent)
                     } else {
                         Snackbar.make(root, "비밀번호는 8~12글자 이내입니다.", Snackbar.LENGTH_SHORT)
