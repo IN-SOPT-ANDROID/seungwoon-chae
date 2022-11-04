b9fe87774f4616a0f5f3004ddf6e76234e410472
CHAE <chaso1495@naver.com>
2022-11-04 23:14:04 +0900

    [UPDATE] 자동 로그인 기능 구현

--- app/src/main/java/org/sopt/sample/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/LoginActivity.kt
@@ -1,22 +1,26 @@
 package org.sopt.sample
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.sopt.sample.databinding.ActivityLoginBinding
 import com.google.android.material.snackbar.Snackbar
+import org.sopt.sample.databinding.ActivityLoginBinding
 import org.sopt.sample.home.HomeActivity
+import org.sopt.sample.home.MySharedPreferences
+
 
 class LoginActivity : AppCompatActivity() {
     private var id: String? = null
     private var pw: String? = null
-    private var mbti: String? = null
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        autologinCheck()
         signupCheck()
 
         binding.btLogin.setOnClickListener() {
@@ -28,20 +32,32 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun signupCheck() {
+    private fun autologinCheck() { // 자동 로그인 기능, sharedPreference에 값이 존재하면 로그인 화면을 뛰어넘어 홈화면으로 진입
+        if(!(MySharedPreferences.getUserId(this).isNullOrBlank())
+            && !(MySharedPreferences.getUserPw(this).isNullOrBlank())){
+                Toast.makeText(this.applicationContext, "자동 로그인 되었습니다", Toast.LENGTH_SHORT).show();
+                val homeIntent = Intent(this, HomeActivity::class.java)
+                homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK // 로그인 성공 시 직전에 발생한 회원가입/로그인 acitivity 삭제처리
+                startActivity(homeIntent)
+            }
+    }
+
+    private fun signupCheck() { // 회원가입 activity에서 넘어오는 상황 감지
         if (intent.hasExtra("id") && intent.hasExtra("pw")) {
             Snackbar.make(binding.root, "회원가입이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
             id = intent.getStringExtra("id")
             pw = intent.getStringExtra("pw")
-            mbti = intent.getStringExtra("mbti")
         }
     }
 
     private fun loginEvent() {
+
         if (binding.etId.text.toString() == id && binding.etPw.text.toString() == pw) {
+            MySharedPreferences.setUserId(this, id.toString())
+            MySharedPreferences.setUserPw(this, pw.toString())
             Toast.makeText(this.applicationContext, "로그인에 성공했습니다", Toast.LENGTH_SHORT).show();
             val homeIntent = Intent(this, HomeActivity::class.java)
-            homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK // 로그인 성공 시 직전에 발생한 회원가입/로그인 acitivity 삭제처리
             startActivity(homeIntent)
         } else {
             Toast.makeText(this.applicationContext, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/org/sopt/sample/home/MySharedPreference.kt
@@ -0,0 +1,40 @@
+package org.sopt.sample.home
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object MySharedPreferences { // 자동 로그인 기능 구현을 위한 SharePreference 객체, 접근의 편의성을 위해 최상위 객체 object로 선언함
+    private const val MY_ACCOUNT : String = "account"
+
+    fun setUserId(context: Context, input: String) { // 유저 아이디 저장
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_ID", input)
+        editor.commit()
+    }
+
+    fun getUserId(context: Context): String { // 저장된 유저 아이디 반환
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_ID", "").toString()
+    }
+
+    fun setUserPw(context: Context, input: String) { // 유저 비밀번호 저장
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_PW", input)
+        editor.commit()
+    }
+
+    fun getUserPw(context: Context): String { // 저장된 유저 비밀번호 반환
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_PW", "").toString()
+    }
+
+    fun clearNowUser(context: Context) { // 로그아웃 기능이 추가될 시 사용할 예정, 자동로그인 상황을 초기화
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.clear()
+        editor.commit()
+    }
+
+}
