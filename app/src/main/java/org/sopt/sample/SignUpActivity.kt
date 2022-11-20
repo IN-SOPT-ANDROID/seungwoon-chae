9f16c501ec5a0741ee6b1320ee070da25b586b8b
Chae's Mac Pro <chaeseung-un@Chaesui-MacBookPro.local>
2022-11-21 01:54:38 +0900

    [FIX] 표현 수정 및 editText 개편 준비

--- app/src/main/java/org/sopt/sample/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/SignUpActivity.kt
@@ -22,54 +22,46 @@ import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
     private val viewModel by viewModels<SignupViewModel>()
-    private var validEmail: Boolean = false
-    private var validPw: Boolean = false
     private lateinit var binding: ActivitySignupBinding
-    val Emailpattern = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,10}$"
-    val Pwpattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{6,12}$"
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.vm = viewModel
         binding.lifecycleOwner = this
-        binding.btSignupend.isEnabled = false // 조건 충족 시에만 버튼 활성화
-        binding.txtPwError.visibility = View.INVISIBLE
-        binding.txtEmailError.visibility = View.INVISIBLE
 
-        viewModel.inputEmail.observe(this){ // 형식 검증 후 실패시 et 밑줄 빨강 + 에러메시지 visible
-            if(validEmailcheck(it)){
-                validEmail = true
-                binding.txtEmailError.visibility = View.INVISIBLE
-                if(validEmail && validPw){
+        viewModel.inputEmailcheck.observe(this){ // 형식 검증 변수 확인, 실패시 et 밑줄 빨강 + 에러메시지 visible
+            if(it){
+                binding.txtEmailError.visibility = View.INVISIBLE // 형식 통과
+                if(viewModel.inputPwcheck.value == true){ // 비번까지 정상이면 버튼 활성화
                     binding.btSignupend.isEnabled = true
                 }
             } else {
-                validEmail = false
-                binding.btSignupend.isEnabled = false
-                if(it.isNotBlank()) {
+                binding.btSignupend.isEnabled = false // 버튼 비활성화
+                if(viewModel.inputEmail.value.equals("")){ // 공백이면 오류메시지 출력하지 않음
+                    binding.txtEmailError.visibility = View.INVISIBLE
+                } else { // 공백이 아니면서 observe 결과가 false면 오류상태
                     binding.txtEmailError.visibility = View.VISIBLE
                 }
             }
         }
 
-        viewModel.inputPw.observe(this){
-            if(validPwcheck(it)){
-                validPw = true
-                binding.txtPwError.visibility = View.INVISIBLE
-                if(validEmail && validPw){
-                    binding.btSignupend.isEnabled = true // 유효성 검사 통과 -> 버튼 활성화
+        viewModel.inputPwcheck.observe(this){
+            if(it){
+                binding.txtPwError.visibility = View.INVISIBLE // 형식 통과
+                if(viewModel.inputEmailcheck.value == true){ // 이메일까지 정상이면 버튼 활성화
+                    binding.btSignupend.isEnabled = true
                 }
             } else {
-                validPw = false
-                binding.btSignupend.isEnabled = false
-                if(it.isNotBlank()) {
+                binding.btSignupend.isEnabled = false // 버튼 비활성화
+                if(viewModel.inputPw.value.equals("")){// 공백이면 오류메시지 출력하지 않음
+                    binding.txtPwError.visibility = View.INVISIBLE
+                } else { // 공백이 아니면서 observe 결과가 false면 오류상태
                     binding.txtPwError.visibility = View.VISIBLE
                 }
             }
-        }
+        } // 확장성이 불안해 보이는 코드. mediatorLiveData를 활용할 수는 없을까?
 
         binding.btSignupend.setOnClickListener {
             viewModel.signup(
@@ -85,92 +77,6 @@ class SignUpActivity : AppCompatActivity() {
         viewModel.serverError.observe(this){ it -> // 에러메시지 출력
             Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
         }
-    }
-
-    // 3개의 input이 모두 공백아 아닌 것이 확인되면 버튼 활성화
-    /*private fun checkAllInputActivated(){
-        with(binding)
-        {
-            etPwNew.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    btSignupend.isEnabled = !etIdNew.text.toString().isBlank() && !etPwNew.text.toString().isBlank() && !etEmailNew.text.toString().isBlank()
-                }
-            })
-            etIdNew.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    btSignupend.isEnabled = !etIdNew.text.toString().isBlank() && !etPwNew.text.toString().isBlank() && !etEmailNew.text.toString().isBlank()
-                }
-            })
-            etEmailNew.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    btSignupend.isEnabled = !etIdNew.text.toString().isBlank() && !etPwNew.text.toString().isBlank() && !etEmailNew.text.toString().isBlank()
-                }
-            })
-        }
-    }
-
-    private fun clickEvent() {
-        with(binding)
-        {
-            btSignupend.setOnClickListener() {
-                // 회원가입이 성공한 id와 pw를 서버에 전달하고 loginActivity로 이동한다.
-                signUpService.signup(
-                    RequestSignUpDTO(
-                        etEmailNew.text.toString(),
-                        etPwNew.text.toString(),
-                        etIdNew.text.toString()
-                    )
-                ).enqueue(object : Callback<ResponseSignUpDTO> {
-                    override fun onResponse(
-                        call: Call<ResponseSignUpDTO>,
-                        response: Response<ResponseSignUpDTO>
-                    ) {
-                        when(response.code()) {
-                            400 -> {
-                                Toast.makeText(this@SignUpActivity, "회원가입 실패: 아이디와 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT)
-                            }
-                            500 ->  {
-                                Toast.makeText(this@SignUpActivity, "회원가입 실패: 서버 오류", Toast.LENGTH_SHORT)
-                            }
-                        }
-                        if(response.isSuccessful) {
-                            startActivity(
-                                Intent(
-                                    this@SignUpActivity,
-                                    LoginActivity::class.java
-                                )
-                            )
-                        }
-                    }
-                    override fun onFailure(call: Call<ResponseSignUpDTO>, t: Throwable) {
-                        Toast.makeText(this@SignUpActivity, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                })
-            }
-        }
-    }*/
-
-    fun validEmailcheck(Email: String): Boolean {
-        val pattern = Pattern.compile(Emailpattern)
-        return pattern.matcher(Email).find()
-    }
-
-    fun validPwcheck(Pw: String): Boolean {
-        val pattern = Pattern.compile(Pwpattern)
-        return pattern.matcher(Pw).find()
     }
 }
 
--- app/src/main/java/org/sopt/sample/viewmodel/SignupViewModel.kt
+++ app/src/main/java/org/sopt/sample/viewmodel/SignupViewModel.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.BaseObservable
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import org.sopt.sample.LoginActivity
 import org.sopt.sample.remote.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.regex.Pattern
 
 class SignupViewModel: ViewModel() {
+    val Emailpattern = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,10}$"
+    val Pwpattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{6,12}$"
+    // 형식 검증을 위한 정규표현식
+
     private val _signupResult: MutableLiveData<ResponseSignUpDTO> = MutableLiveData()
     val signupResult: LiveData<ResponseSignUpDTO> = _signupResult
     private val signupService = ServicePool.singupService
@@ -25,6 +28,14 @@ class SignupViewModel: ViewModel() {
     val inputEmail = MutableLiveData<String>().apply { value = "" }
     val inputPw = MutableLiveData<String>().apply { value = "" }
     val inputId = MutableLiveData<String>().apply { value = "" }
+
+    val inputEmailcheck : LiveData<Boolean> = Transformations.map(inputEmail) { email ->
+        validEmailcheck(email)
+    } // map을 활용해 실시간으로 변하는 input을 검증하여 Boolean 변수를 반환
+
+    val inputPwcheck : LiveData<Boolean> = Transformations.map(inputPw) { pw ->
+        validPwcheck(pw)
+    }
 
     fun signup(email: String, password: String, id: String){
         signupService.signup(
@@ -42,13 +53,21 @@ class SignupViewModel: ViewModel() {
                     _successSignup.value = false
                 }
             }
+
             override fun onFailure(call: Call<ResponseSignUpDTO>, t: Throwable) { // 서버통신 자체가 실패
                 _serverError.value = t.message
                 _successSignup.value = false
             }
-        }
-        )
+        })
     }
 
+    private fun validEmailcheck(Email: String): Boolean { // 이메일 형식 검증
+        val pattern = Pattern.compile(Emailpattern)
+        return pattern.matcher(Email).find()
+    }
 
+    private fun validPwcheck(Pw: String): Boolean { // 비밀번호 형식 검증
+        val pattern = Pattern.compile(Pwpattern)
+        return pattern.matcher(Pw).find()
+    }
 }
